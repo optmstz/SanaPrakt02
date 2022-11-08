61c3d718e48ccdf6e6c17b06638f7558f58f2d11
Viktor Yelenetskyi <ipz223_yevv@student.ztu.edu.ua>
2022-11-08 15:20:09 +0200

    Added 4 new line

--- Task1/SanaPrakt02/SanaPrakt02.cpp
+++ Task1/SanaPrakt02/SanaPrakt02.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Course Sana Commerce Development Bootcamp\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
