94596cc745202b7aed9227010e9b5392e9409b2a
Viktor Yelenetskyi <ipz223_yevv@student.ztu.edu.ua>
2022-11-08 15:44:19 +0200

    Changed last 2 lines

--- Task1/SanaPrakt02/SanaPrakt02.cpp
+++ Task1/SanaPrakt02/SanaPrakt02.cpp
@@ -7,8 +7,8 @@ int main()
     std::cout << "1\n";
     std::cout << "2\n";
     std::cout << "3\n";
-    std::cout << "Course Sana Commerce Development Bootcamp\n";
-    std::cout << "Course Sana Commerce Development Bootcamp\n";
+    std::cout << "Time\n";
+    std::cout << "Time\n";
 }
 
 
